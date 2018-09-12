3b8a403f4e16b54fb2d462570e8a11c581895468
Peter Hunt <naseberry@users.noreply.github.com>
2018-09-12 16:42:53 +0100

    Remove placeholder text and add set up content

--- src/js/mde.js
+++ src/js/mde.js
@@ -1,8 +1,8 @@
 let simplemde = new SimpleMDE({
   element: document.getElementById('mde'),
   autofocus: true,
-  toolbar: [ 'bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', 'table', 'horizontal-rule', '|', 'preview', 'side-by-side', 'fullscreen', '|', 'guide', '|', 'code' ],
-  placeholder: 'Welcome to AME'
+  toolbar: [ 'bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', 'table', 'horizontal-rule', '|', 'preview', 'side-by-side', 'fullscreen', '|', 'guide', '|', 'code' ]
 });
 
+simplemde.value("# Welcome to AME\n A simple cross platform Markdown editor built with [Electron](https://electronjs.org/) and [SimpleMDE](https://simplemde.com/)"); // set start-up content
 simplemde.toggleSideBySide(simplemde); // open preview on load
